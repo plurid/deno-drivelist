0f310ff949fe0a5e7aac04f6cf09db5cde91ab63
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-11-30 09:34:04 +0200

    setup: package

--- compile.sh
+++ compile.sh
@@ -1,11 +1,33 @@
+mkdir -p build/binary/darwin
+
+
 clang++ -arch x86_64 -c -std=c++17 \
     -o build/drivelist.o \
     -I./vendor \
     source/binary/drivelist.cpp
 
+clang++ -arch x86_64 -stdlib=libc++ -std=c++11 \
+  -x objective-c++ \
+  -c source/binary/darwin/list.mm \
+  -o build/binary/darwin/list.o \
+  -I. \
+  -I./vendor \
+  -I/usr/include \
+  -I/System/Library/Frameworks/CoreFoundation.framework/Headers
+
+clang++ -arch x86_64 -stdlib=libc++ -std=c++11 \
+  -x objective-c++ \
+  -c source/binary/darwin/REDiskList.m \
+  -o build/binary/darwin/REDiskList.o \
+  -I. \
+  -I./vendor \
+  -I/usr/include \
+  -I/System/Library/Frameworks/CoreFoundation.framework/Headers
 
 clang++ -shared -arch x86_64 -stdlib=libc++ \
     ./build/drivelist.o \
+    ./build/binary/darwin/list.o \
+    ./build/binary/darwin/REDiskList.o \
     -framework Carbon \
     -framework DiskArbitration \
     -framework Foundation \
